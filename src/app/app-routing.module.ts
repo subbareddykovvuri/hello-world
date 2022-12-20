5686b0490ae912c9ef7518c5c3576c78a710ddb1
subbareddykovvuri <subbareddy934@gmail.com>
2022-12-20 23:42:51 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroComponent } from './Components/intro/intro.component';
+import { EmpInfoComponent } from './emp-info/emp-info.component';
+import { FormComponentComponent } from './form-component/form-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login',component:FormComponentComponent},
+  {path: 'home',component:IntroComponent},
+  {path: 'about',component:EmpInfoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
