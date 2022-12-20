4207a1a5992edaceb44c3346cafa89529e083f33
subbareddykovvuri <subbareddy934@gmail.com>
2022-12-20 23:21:33 +0530

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { EmpInfoComponent } from './emp-info/emp-info.component';
 import { FormComponentComponent } from './form-component/form-component.component';
 import { FormsModule } from '@angular/forms';
 import { EInfoComponent } from './e-info/e-info.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { IntroComponent } from './Components/intro/intro.component';
+import { CoursesComponent } from './Components/courses/courses.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import { EInfoComponent } from './e-info/e-info.component';
     ImagecomponentComponent,
     EmpInfoComponent,
     FormComponentComponent,
-    EInfoComponent
+    EInfoComponent,
+    NavbarComponent,
+    IntroComponent,
+    CoursesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
